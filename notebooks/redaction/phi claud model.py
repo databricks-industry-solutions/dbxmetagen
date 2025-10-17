dade7a8661a7fdc9cafca99dbc301fd20b997ce7
btbeal-db <brennan.beal@databricks.com>
2025-10-17 21:37:14 +0000

    Adding ai_query to eval df

--- notebooks/redaction/phi claud model.py
+++ notebooks/redaction/phi claud model.py
@@ -5,8 +5,41 @@
 # COMMAND ----------
 
 import json
+import re
+import pandas as pd
+from pyspark.sql.functions import pandas_udf, col, expr, lit, concat
+from pyspark.sql.types import LongType, StringType
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import expr, lit, concat
+
+# COMMAND ----------
+
+dbutils.widgets.text(
+    defaultValue="dbxmetagen.default.phi_test_data",
+    label="0. Table",
+    name="medical_text_table"
+)
+
+dbutils.widgets.text(
+    defaultValue="medical_text_with_phi",
+    label="1. Column containing PHI",
+    name="medical_text_col"
+)
+
+dbutils.widgets.dropdown(
+  name='endpoint',
+  defaultValue='databricks-gpt-oss-120b',
+  choices = sorted([ 'databricks-gpt-oss-20b',
+                    'databricks-gpt-oss-120b',
+                    'databricks-gemma-3-12b',
+                    'databricks-llama-4-maverick',
+                    'databricks-meta-llama-3-3-70b-instruct',
+                    'databricks-meta-llama-3-1-8b-instruct']),
+  label = '1. Endpoint'
+)
+
+med_text_table = dbutils.widgets.get("medical_text_table")
+med_text_col = dbutils.widgets.get("medical_text_col")
+endpoint = dbutils.widgets.get("endpoint")
 
 # COMMAND ----------
 
@@ -18,9 +51,6 @@ display(df)
 
 # COMMAND ----------
 
-spark = SparkSession.builder.getOrCreate()
-
-df = spark.table("dbxmetagen.default.phi_test_data")
 df.repartition(50).write.format("delta").mode("overwrite").saveAsTable("dbxmetagen.default.phi_test_data_restructured")
 df_stream = spark.readStream.format("delta").option("maxBytesPerTrigger", "50K").table("dbxmetagen.default.phi_test_data_restructured")
 
@@ -56,44 +86,9 @@ df_stream = df.withColumn("presdio_pii_entities", extract_entities_udf(df["medic
 
 # COMMAND ----------
 
-# MAGIC %tb
-
-# COMMAND ----------
-
-spark = SparkSession.builder.getOrCreate()
-
-df = spark.createDataFrame([
-  {"medical_text": "Some med text", "id": 1},
-  {"medical_text": "Some med text", "id": 2}
-  ])
-
-(
-    df
-    .write
-    .format("delta")
-    .mode("overwrite")
-    .saveAsTable("dbxmetagen.default.dummy_phi")
-)
-
-
-
-# COMMAND ----------
-
 from pyspark.sql.types import *
 
 spark = SparkSession.builder.getOrCreate()
-
-# schema = StructType([
-#     StructField("medical_text", StringType(), True),
-#     StructField("ID", IntegerType(), True),
-#     StructField("City", StringType(), True)
-# ])
-
-
-
-#df = spark.table("dbxmetagen.default.phi_test_data")
-# df.repartition(50).write.format("delta").mode("overwrite").saveAsTable("dbxmetagen.default.phi_test_data_restructured")
-# df_stream = spark.readStream.format("delta").option("maxBytesPerTrigger", "50K").table("dbxmetagen.default.phi_test_data_restructured")
 
 from presidio_analyzer import AnalyzerEngine
 from pyspark.sql.functions import pandas_udf
@@ -200,6 +195,104 @@ df_transformed = df_stream.select(
         }}'
     )""").alias("extracted_entities_list")
 )
+
+# COMMAND ----------
+
+# ==== BTBeal AI Query 
+prompt = """
+You are an expert in Protected Health Information (PHI) detection who will help identify all PHI entities in a piece of medical text.
+
+Qualifying PHI includes:
+1. Names;
+2. All geographical subdivisions smaller than a State, including street address, city, county, precinct, zip code, and their equivalent geocodes, except for the initial three digits of a zip code, if according to the current publicly available data from the Bureau of the Census: (1) The geographic unit formed by combining all zip codes with the same three initial digits contains more than 20,000 people; and (2) The initial three digits of a zip code for all such geographic units containing 20,000 or fewer people is changed to 000.
+3. All elements of dates (except year) for dates directly related to an individual, including birth date, admission date, discharge date, date of death; and all ages over 89 and all elements of dates (including year) indicative of such age, except that such ages and elements may be aggregated into a single category of age 90 or older;
+4. Phone numbers;
+5. Fax numbers;
+6. Electronic mail addresses;
+7. Social Security numbers;
+8. Medical record numbers;
+9. Health plan beneficiary numbers;
+10. Account numbers;
+11. Certificate/license numbers;
+12. Vehicle identifiers and serial numbers, including license plate numbers;
+13. Device identifiers and serial numbers;
+14. Web Universal Resource Locators (URLs);
+15. Internet Protocol (IP) address numbers;
+16. Biometric identifiers, including finger and voice prints;
+17. Full face photographic images and any comparable images; and
+18. Any other unique identifying number, characteristic, or code (note this does not mean the unique code assigned by the investigator to code the data)
+There are also additional standards and criteria to protect individuals from re-identification. Any code used to replace the identifiers in data sets cannot be derived from any information related to the individual and the master codes, nor can the method to derive the codes be disclosed. For example, a subjects initials cannot be used to code their data because the initials are derived from their name. Additionally, the researcher must not have actual knowledge that the research subject could be re-identified from the remaining identifiers in the PHI used in the research study. In other words, the information would still be considered identifiable if there was a way to identify the individual even though all of the 18 identifiers were removed.
+
+You will identify all PHI with the following enums as the "label":
+
+["person",
+"phone",
+"email",
+"SSN",
+"medical record number",
+"date of birth",
+"street address",
+"geographic identifier"]
+
+and respond with a list of dictionaries with the actual text, its label, and a numeric value of how confident you are that this is PHI, such as [{"text": "Brennan Beal", "label": "Person", "score": 1.0}, {"text": "123-45-6789", "label": "SSN", "score": 0.99}]
+
+If you are not sure, you can add a lower score between 0 and 1.0.
+
+The text is listed here: 
+<MedicalText>
+{med_text}
+<MedicalText/>
+
+EXAMPLE: 
+MedicalText: "MRN: 222345 -- I saw patient Brennan Beal today at 11:30am, who presents with a sore throat and temperature of 103F"
+response: [{"text": "Brennan Beal", "label": "person", "score": 1.0}, {"text": "222345", "label": "medical record number", "score": 0.99}]
+"""
+
+query = f"""
+  WITH data_with_prompting AS (
+      SELECT *,
+            REPLACE('{prompt}', '{{med_text}}', CAST({med_text_col} AS STRING)) AS prompt
+      FROM {med_text_table}
+  )
+  SELECT *,
+        ai_query(
+          endpoint => '{endpoint}',
+          request => prompt,
+          failOnError => false,
+          returnType => 'STRUCT<result: ARRAY<STRUCT<text: STRING, label: STRING, score: STRING>>>',
+          modelParameters => named_struct('reasoning_effort', 'low')
+        ) AS response
+  FROM data_with_prompting
+  """
+
+@pandas_udf(StringType())
+def add_positions_to_entities_udf(identified_entities_series, sentences):
+    new_entity_series = []
+    for entity_list, sentence in zip(identified_entities_series, sentences):
+        entities = json.loads(entity_list)
+        for entity in entities:
+            pattern = re.escape(entity['text'])
+            positions = [(m.start(), m.end() - 1) for m in re.finditer(pattern, sentence)]
+            entity['positions'] = positions
+        
+        new_entity_series.append(json.dumps(entities
+                                            ))
+    return pd.Series(new_entity_series)
+
+ai_query_df = (
+  spark
+    .sql(query)
+    .withColumn(
+        "phi_ai_query_entities", 
+        add_positions_to_entities_udf(
+            F.col("response.result"), F.col("medical_text_with_phi")
+        )
+    )
+)
+
+# COMMAND ----------
+
+display(ai_query_df)
 
 # COMMAND ----------
 
